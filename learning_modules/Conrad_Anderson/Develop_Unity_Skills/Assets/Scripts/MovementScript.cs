ae03e6b4259f2862feac551bb9e4b3ed88e559ef
Conrad Anderson <wdarnoc05@gmail.com>
2022-06-24 10:53:49 -0400

    IT WORKED

---
+++ learning_modules/Conrad_Anderson/Develop_Unity_Skills/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * -1);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.left * Time.deltaTime * speed * =1);
+
+    }
+}
