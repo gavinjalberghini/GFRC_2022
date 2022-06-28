4007073d3e485d012e12ea823e70c43f49700228
PhucXDoan <phucxdoan@gmail.com>
2022-06-27 20:47:22 -0400

    Basic gamepad support for robot behavior.

--- GFRC_2022/Assets/Behaviors/mechanum_drive.cs
+++ GFRC_2022/Assets/Behaviors/mechanum_drive.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class mechanum_drive : MonoBehaviour
 {
@@ -12,6 +13,13 @@ public class mechanum_drive : MonoBehaviour
 	GameObject robot_pivot_indicator;
 	Rigidbody  robot_base_rigid_body;
 	Vector2    pivot_offset;
+
+	// @TODO@ Either Unity is buggy or I'm using the Input System API wrong. `Gamepad.current.buttonWest.wasPressedThisFrame` may trigger multiple times, instead of once.
+	// State of the button will have to be saved in order to side step this issue, although it's pretty crufty.
+	bool button_west;
+	bool button_east;
+	bool button_south;
+	bool button_north;
 
 	void Start()
 	{
@@ -26,10 +34,11 @@ public class mechanum_drive : MonoBehaviour
 		// Pivot change.
 		//
 
-		if (Input.GetKeyDown(KeyCode.LeftArrow )) { pivot_offset.x -= 1.0f; }
-		if (Input.GetKeyDown(KeyCode.RightArrow)) { pivot_offset.x += 1.0f; }
-		if (Input.GetKeyDown(KeyCode.DownArrow )) { pivot_offset.y -= 1.0f; }
-		if (Input.GetKeyDown(KeyCode.UpArrow   )) { pivot_offset.y += 1.0f; }
+		// @TODO@ Make the pivot change depending on the orientation of the camera (e.g. pressing "up" would make the pivot go up, even when it is going lower on the base).
+		if (Keyboard.current[Key.LeftArrow ].wasPressedThisFrame || Gamepad.current.buttonWest.isPressed  && !button_west ) { pivot_offset.x -= 1.0f; }
+		if (Keyboard.current[Key.RightArrow].wasPressedThisFrame || Gamepad.current.buttonEast.isPressed  && !button_east ) { pivot_offset.x += 1.0f; }
+		if (Keyboard.current[Key.DownArrow ].wasPressedThisFrame || Gamepad.current.buttonSouth.isPressed && !button_south) { pivot_offset.y -= 1.0f; }
+		if (Keyboard.current[Key.UpArrow   ].wasPressedThisFrame || Gamepad.current.buttonNorth.isPressed && !button_north) { pivot_offset.y += 1.0f; }
 		pivot_offset.x = Mathf.Clamp(pivot_offset.x, -1.0f, 1.0f);
 		pivot_offset.y = Mathf.Clamp(pivot_offset.y, -1.0f, 1.0f);
 
@@ -42,11 +51,18 @@ public class mechanum_drive : MonoBehaviour
 		// Cardinal movement.
 		//
 
-		Vector2 movement = new Vector2(0.0f, 0.0f);
-		if (Input.GetKey(KeyCode.A)) { movement.x -= 1.0f; }
-		if (Input.GetKey(KeyCode.D)) { movement.x += 1.0f; }
-		if (Input.GetKey(KeyCode.S)) { movement.y -= 1.0f; }
-		if (Input.GetKey(KeyCode.W)) { movement.y += 1.0f; }
+		Vector2 movement = Gamepad.current.leftStick.ReadValue();
+		if (movement == new Vector2(0.0f, 0.0f))
+		{
+			if (Keyboard.current[Key.A].isPressed) { movement.x -= 1.0f; }
+			if (Keyboard.current[Key.D].isPressed) { movement.x += 1.0f; }
+			if (Keyboard.current[Key.S].isPressed) { movement.y -= 1.0f; }
+			if (Keyboard.current[Key.W].isPressed) { movement.y += 1.0f; }
+			if (movement != new Vector2(0.0f, 0.0f))
+			{
+				movement = Vector3.Normalize(movement);
+			}
+		}
 		movement *= movement_force;
 		movement *= Mathf.Max(max_speed - Vector3.Magnitude(robot_base_rigid_body.velocity), 0.0f); // @NOTE@ Stops from forever accelerating.
 		robot_base_rigid_body.AddForce(robot_base.transform.right * movement.x + robot_base.transform.forward * movement.y);
@@ -56,13 +72,24 @@ public class mechanum_drive : MonoBehaviour
 		//
 
 		// @TODO@ Change this to use forces?
-		float rotation_amount = 0.0f;
-		if (Input.GetKey(KeyCode.Q)) { rotation_amount -= 1.0f; }
-		if (Input.GetKey(KeyCode.E)) { rotation_amount += 1.0f; }
+		float rotation_amount = Gamepad.current.rightStick.ReadValue().x;
+		if (rotation_amount == 0.0f)
+		{
+			if (Keyboard.current[Key.Q].isPressed) { rotation_amount -= 1.0f; }
+			if (Keyboard.current[Key.E].isPressed) { rotation_amount += 1.0f; }
+		}
 		rotation_amount *= torque * Time.deltaTime;
 
 		robot_base.transform.RotateAround(pivot_indicator_position, Vector3.up, rotation_amount);
 
+		//
+		// Misc.
+		//
+
 		robot_pivot_indicator.transform.position = pivot_indicator_position;
+		button_west                              = Gamepad.current.buttonWest.isPressed;
+		button_east                              = Gamepad.current.buttonEast.isPressed;
+		button_south                             = Gamepad.current.buttonSouth.isPressed;
+		button_north                             = Gamepad.current.buttonNorth.isPressed;
 	}
 }
