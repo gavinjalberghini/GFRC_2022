eaa46483e064e3cb8e0663942ef1f23b3b193538
PhucXDoan <phucxdoan@gmail.com>
2022-07-12 17:25:34 -0400

    Fixed camera orientation mode.

--- GFRC_2022/Assets/Camera.cs
+++ GFRC_2022/Assets/Camera.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEditor;
 using static Global;
 
 public class Camera : MonoBehaviour
 {
+	public enum RotMode
+	{
+		free,
+		focused,
+		fix
+	};
+
 	public float      sensitivity     = 0.2f;
 	public float      dampening_rot   = 0.1f;
 	public float      dampening_pos   = 0.01f;
-	public bool       is_focused      = true;
+	public RotMode    rot_mode        = RotMode.free;
 	public bool       is_relative_pos = false;
 	public Transform  subject         = null;
 
 	Vector2    target_yaw_pitch     = new Vector2(0.0f, 0.0f);
 	Vector2    yaw_pitch            = new Vector2(0.0f, 0.0f);
-	Quaternion initial_orientation  = Quaternion.identity;
+	Quaternion initial_rot          = Quaternion.identity;
 	Vector3    initial_relative_pos = new Vector3(0.0f, 0.0f, 0.0f);
 
 	void OnValidate()
 	{
 		sensitivity         = Mathf.Clamp(sensitivity, 0.01f, 1.0f);
 		dampening_rot       = Mathf.Clamp(dampening_rot, 0.0f, 1.0f);
-		initial_orientation = transform.rotation;
 
 		if (subject != null)
 		{
@@ -33,6 +40,7 @@ public class Camera : MonoBehaviour
 	void Start()
 	{
 		Cursor.lockState = CursorLockMode.Locked;
+		initial_rot      = transform.rotation;
 	}
 
 	void Update()
@@ -47,29 +55,33 @@ public class Camera : MonoBehaviour
 		yaw_pitch.x        = mod(       yaw_pitch.x, 360.0f);
 		yaw_pitch.y        = mod(       yaw_pitch.y, 360.0f);
 
-		if (key_now_down(Key.Tab))
-		{
-			is_focused       = !is_focused;
-			target_yaw_pitch = yaw_pitch;
-		}
+		// @NOTE@ Ref. for how to toggle between fixed and focused.
+		//if (key_now_down(Key.Tab))
+		//{
+		//	is_focused       = !is_focused;
+		//	target_yaw_pitch = yaw_pitch;
+		//}
 
-		transform.localRotation = initial_orientation;
+		transform.localRotation = initial_rot;
 
-		if (is_focused && subject != null)
-		{
-			Quaternion look  = Quaternion.LookRotation(Quaternion.Inverse(initial_orientation) * (subject.position - transform.position), transform.up);
-			target_yaw_pitch = new Vector2(look.eulerAngles.y, look.eulerAngles.x);
-		}
-		else
+		if (rot_mode == RotMode.free)
 		{
 			target_yaw_pitch   += new Vector2(mouse_delta().x * sensitivity, -mouse_delta().y * sensitivity);
 			target_yaw_pitch.y  = Mathf.Clamp(min_degree_arc(0.0f, target_yaw_pitch.y), -80.0f, 80.0f);
 		}
+		else if (rot_mode == RotMode.focused && subject != null)
+		{
+			Quaternion look  = Quaternion.LookRotation(Quaternion.Inverse(initial_rot) * (subject.position - transform.position), transform.up);
+			target_yaw_pitch = new Vector2(look.eulerAngles.y, look.eulerAngles.x);
+		}
+		else if (rot_mode == RotMode.fix)
+		{
+		}
 
-		yaw_pitch.x += dampen(0.0f, min_degree_arc(yaw_pitch.x, target_yaw_pitch.x), dampening_rot / (is_focused ? 10.0f : 1000.0f));
-		yaw_pitch.y += dampen(0.0f, min_degree_arc(yaw_pitch.y, target_yaw_pitch.y), dampening_rot / (is_focused ? 10.0f : 1000.0f));
+		yaw_pitch.x += dampen(0.0f, min_degree_arc(yaw_pitch.x, target_yaw_pitch.x), dampening_rot);
+		yaw_pitch.y += dampen(0.0f, min_degree_arc(yaw_pitch.y, target_yaw_pitch.y), dampening_rot);
 
-		transform.localRotation = Quaternion.AngleAxis(yaw_pitch.x, transform.up) * Quaternion.AngleAxis(yaw_pitch.y, transform.right) * initial_orientation;
+		transform.localRotation = Quaternion.AngleAxis(yaw_pitch.x, transform.up) * Quaternion.AngleAxis(yaw_pitch.y, transform.right) * initial_rot;
 	}
 }
 
