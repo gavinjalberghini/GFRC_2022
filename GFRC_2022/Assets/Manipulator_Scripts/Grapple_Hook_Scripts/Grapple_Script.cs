63885ac6afd70e6079628a6356b7207be731c5d3
Conrad Anderson <wdarnoc05@gmail.com>
2022-07-14 17:52:35 -0400

    Updated Grapple Manipulator and Grapple_Script.cs

--- GFRC_2022/Assets/Manipulator_Scripts/Grapple_Hook_Scripts/Grapple_Script.cs
+++ GFRC_2022/Assets/Manipulator_Scripts/Grapple_Hook_Scripts/Grapple_Script.cs
@@ -22,17 +22,23 @@ public class Grapple_Script : MonoBehaviour
     void Update()
     {
 
-        if(Gamepad.current.leftTrigger.wasPressedThisFrame)
+        if(Gamepad.current.leftTrigger.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame)
         {
-            this.gameObject.GetComponent<LineRenderer>().enabled = true;
             StartGrapple();
         }
 
-        if(Gamepad.current.leftTrigger.wasReleasedThisFrame)
+
+        if (Gamepad.current.leftTrigger.wasReleasedThisFrame || Mouse.current.leftButton.wasReleasedThisFrame)
         {
-            this.gameObject.GetComponent<LineRenderer>().enabled = false;
-            StopGrapple();
+                StopGrapple();
         }
+
+        if (Gamepad.current.leftTrigger.ReadValue() > 0.1f || Mouse.current.leftButton.ReadValue() > 0.1f)
+        {
+            WhileGrappling();
+        }
+
+       
 
     }
 
@@ -44,7 +50,7 @@ public class Grapple_Script : MonoBehaviour
     void StartGrapple()
     {
         RaycastHit hit;
-        if(Physics.Raycast(grappleTip.position, grappleTip.forward, out hit, maxDistance))
+        if(Physics.Raycast(grappleTip.position, grappleTip.forward, out hit, maxDistance, WhatIsGrappleable))
         {
             grapplePoint = hit.point;
             joint = robot.gameObject.AddComponent<SpringJoint>();
@@ -56,16 +62,36 @@ public class Grapple_Script : MonoBehaviour
             joint.maxDistance = 0.8f;
             joint.minDistance = 0.2f;
 
-            joint.spring = 8.0f;
-            joint.damper = 7f;
-            joint.massScale = 4.5f;
 
-            robot.GetComponent<Rigidbody>().freezeRotation = true;
+            lr.positionCount = 2;
+        }
+    }
+
+    void WhileGrappling()
+    {
+        if (Gamepad.current.leftShoulder.ReadValue() > 0f || Mouse.current.rightButton.ReadValue() > 0f)
+        {
+            robot.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePosition;
+            //Debug.Log("moving");
+            //NOTE: AdJUST VALUES IF MASS OF ROBOT IS DIFFERENT
+            joint.spring = 1000.0f;//force robot is moved towards point
+            joint.damper = 7.5f;//force acting against spring
+            joint.massScale = 4.5f;
+        }
+        else if(Gamepad.current.leftShoulder.wasReleasedThisFrame || Mouse.current.rightButton.wasReleasedThisFrame)
+        {
+            robot.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            //Debug.Log("frozen");
+
         }
     }
 
     void DrawGrapple()
     {
+        if(!joint)
+        {
+            return;
+        }
 
         lr.SetPosition(0, grappleTip.position);
         lr.SetPosition(1, grapplePoint);
@@ -74,6 +100,10 @@ public class Grapple_Script : MonoBehaviour
 
     void StopGrapple()
     {
+        lr.positionCount = 0;
         Destroy(joint);
+        //Debug.Log("unfrozen");
+        robot.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
     }
+
 }
