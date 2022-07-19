357d2c6273a463973d4a30de2276cb3d3eb3d84e
PhucXDoan <phucxdoan@gmail.com>
2022-07-19 17:36:18 -0400

    Cleaned up database procedures.

--- GFRC_2022/Assets/Main.cs
+++ GFRC_2022/Assets/Main.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Mono.Data.Sqlite;
+using static Global;
 
 public class Main : MonoBehaviour
 {
@@ -9,11 +12,116 @@ public class Main : MonoBehaviour
 	public Hangar hangar_blue;
 	public Hangar hangar_red;
 
+	//[Header("Debug Display")]
+	//[SerializeField] int score_hub_top_red;
+	//[SerializeField] int score_hub_bot_red;
+	//[SerializeField] int score_hangar_red;
+	//[SerializeField] int score_hub_top_blue;
+	//[SerializeField] int score_hub_bot_blue;
+	//[SerializeField] int score_hangar_blue;
+
 	void Update()
 	{
-		Debug.Log
-		(
-			"\nRed  hangar  : " + hangar_red.calc_score()
-		);
+		//score_hub_top_red  = hub_top.redScore;
+		//score_hub_bot_red  = hub_bot.redScore;
+		//score_hangar_red   = hangar_red.calc_score();
+		//score_hub_top_blue = hub_top.blueScore;
+		//score_hub_bot_blue = hub_bot.blueScore;
+		//score_hangar_blue  = hangar_blue.calc_score();
+	}
+
+	const string DATABASE_URI_NAME = "URI=file:scores.db";
+
+	public void db_wipe()
+	{
+		using (var connection = new SqliteConnection(DATABASE_URI_NAME))
+		{
+			connection.Open();
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText =
+					"DROP TABLE IF EXISTS scores;\n" +
+					"CREATE TABLE scores (id INTEGER PRIMARY KEY AUTOINCREMENT, alliance VARCHAR(16), team INT, points INT);";
+				command.ExecuteNonQuery();
+			}
+			connection.Close();
+		}
+	}
+
+	public void db_set_points(string alliance, int team, int points)
+	{
+		using (var connection = new SqliteConnection(DATABASE_URI_NAME))
+		{
+			connection.Open();
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText =
+					"INSERT OR REPLACE INTO scores (id, alliance, team, points) VALUES\n" +
+						"((SELECT id FROM scores WHERE alliance = \"" + alliance + "\" AND team = " + team + "), \"" + alliance + "\", \"" + team + "\", \"" + points + "\");\n";
+				command.ExecuteNonQuery();
+			}
+			connection.Close();
+		}
+	}
+
+	public (bool, int) db_try_get_points(string alliance, int team)
+	{
+		bool success = false;
+		int  points  = 0;
+
+		using (var connection = new SqliteConnection(DATABASE_URI_NAME))
+		{
+			connection.Open();
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "SELECT alliance, team, points FROM scores WHERE alliance = '" + alliance + "' AND team = '" + team + "';";
+				using (var reader = command.ExecuteReader())
+				{
+					if (reader.Read())
+					{
+						success = true;
+						points  = int.Parse(reader["points"].ToString());
+					}
+					reader.Close();
+				}
+			}
+			connection.Close();
+		}
+
+		return (success, points);
+	}
+
+	public void db_add_points(string alliance, int team, int addend)
+	{
+		(bool success, int points) = db_try_get_points(alliance, team);
+		if (success)
+		{
+			db_set_points(alliance, team, points + addend);
+		}
+		else
+		{
+			db_set_points(alliance, team, addend);
+		}
+	}
+
+	public void db_print_points()
+	{
+		using (var connection = new SqliteConnection(DATABASE_URI_NAME))
+		{
+			connection.Open();
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "SELECT * FROM scores;";
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						print(":: " + reader["alliance"] + " " + reader["team"] + " " + reader["points"]);
+					}
+					reader.Close();
+				}
+			}
+			connection.Close();
+		}
 	}
 }
